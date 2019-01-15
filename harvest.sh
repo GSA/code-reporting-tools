2f339861a81ca1a603153d8436cbc17e8b49d4ec
Jeff Fredrickson <jeffrey.fredrickson@gsa.gov>
2019-01-15 16:44:08 -0500

    add harvesting script

---
+++ harvest.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+output_file=$1
+
+if [ -z "${output_file}" ] ; then
+  echo "Usage: $0 OUTPUT_FILE"
+  exit 1
+fi
+
+if [ -e "${output_file}" ] ; then
+  echo "${output_file}: File already exists; refusing to overwrite"
+  exit 1
+fi
+
+if [ -z "${GITHUB_ACCESS_TOKEN}" ] ; then
+  echo "Please set your GITHUB_ACCESS_TOKEN environment variable."
+  exit 1
+fi
+
+timestamp=$(date +%Y%M%d%H%M%S)
+
+echo -n "Building inventory for GSA organization... "
+codeinv github GSA -a ${GITHUB_ACCESS_TOKEN} > /tmp/gsa.${timestamp}.json
+echo "/tmp/gsa.${timestamp}.json"
+echo -n "Building inventory for 18F organization... "
+codeinv github 18F -a ${GITHUB_ACCESS_TOKEN} > /tmp/18f.${timestamp}.json
+echo "/tmp/18f.${timestamp}.json"
+echo -n "Combining files... "
+codeinv combine GSA -r /tmp/gsa.${timestamp}.json /tmp/18f.${timestamp}.json > ${output_file}
+echo "${output_file}"
+echo "Done!"
