ff35da7608a6ba3e9c7c0e5e4b8c32c722cbf2d1
Jeff Fredrickson <jeffrey.fredrickson@gsa.gov>
2019-04-23 14:07:46 -0400

    add PIF and USWDS

--- harvest.sh
+++ harvest.sh
@@ -19,13 +19,23 @@ fi
 
 timestamp=$(date +%Y%M%d%H%M%S)
 
-echo -n "Building inventory for GSA organization... "
+echo -n "Building inventory for 'GSA' organization... "
 codeinv github GSA -a ${GITHUB_ACCESS_TOKEN} > /tmp/gsa.${timestamp}.json
 echo "/tmp/gsa.${timestamp}.json"
-echo -n "Building inventory for 18F organization... "
+
+echo -n "Building inventory for '18F' organization... "
 codeinv github 18F -a ${GITHUB_ACCESS_TOKEN} > /tmp/18f.${timestamp}.json
 echo "/tmp/18f.${timestamp}.json"
+
+echo -n "Building inventory for 'presidential-innovation-fellows' organization... "
+codeinv github presidential-innovation-fellows -n PIF -a ${GITHUB_ACCESS_TOKEN} > /tmp/pif.${timestamp}.json
+echo "/tmp/18f.${timestamp}.json"
+
+echo -n "Building inventory for 'USWDS' organization... "
+codeinv github USWDS -a ${GITHUB_ACCESS_TOKEN} > /tmp/uswds.${timestamp}.json
+echo "/tmp/18f.${timestamp}.json"
+
 echo -n "Combining files... "
-codeinv combine GSA -r /tmp/gsa.${timestamp}.json /tmp/18f.${timestamp}.json > ${output_file}
+codeinv combine GSA -r /tmp/gsa.${timestamp}.json /tmp/18f.${timestamp}.json /tmp/pif.${timestamp}.json /tmp/uswds.${timestamp}.json > ${output_file}
 echo "${output_file}"
 echo "Done!"
