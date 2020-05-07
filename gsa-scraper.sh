a40629b4856d5149474e65d78e0db9c0695f54ba
Jeff Fredrickson <jeffrey.fredrickson@gsa.gov>
2020-05-07 13:10:52 -0600

    don't commit JSON output files

--- gsa-scraper.sh
+++ gsa-scraper.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # Run scraper script on each GSA GitHub organization
 
-scraper --config config.gsa.json --output-filename code.gsa.json
-scraper --config config.18f.json --output-filename code.18f.json
-scraper --config config.pif.json --output-filename code.pif.json
-scraper --config config.uswds.json --output-filename code.uswds.json
+scraper --config config.gsa.json --output-filename code.gsa.json --skip-labor-hours
+scraper --config config.18f.json --output-filename code.18f.json --skip-labor-hours
+scraper --config config.pif.json --output-filename code.pif.json --skip-labor-hours
+scraper --config config.uswds.json --output-filename code.uswds.json --skip-labor-hours
